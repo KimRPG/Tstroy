82ebff2ea30db711e094bc6eb1edc41a91fbebbb
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-11-26 12:24:02 +0900

    Pathvariable controller

--- src/main/java/com/example/demo/controller/PathVariableController.java
+++ src/main/java/com/example/demo/controller/PathVariableController.java
@@ -1,2 +1,22 @@
-package com.example.demo.controller;public class PathVariableController {
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PathVariableController {
+    @RequestMapping("/path/{name}")
+    public String pathVariable(@PathVariable("name") String userName) {
+        return "PathVariable 연습 name : " + userName;
+    }
+    @RequestMapping("/path/{name}")
+    public String pathVariableV2(@PathVariable String name) {
+        return "PathVariable 연습 name : " + name;
+    }
+
+    @RequestMapping("/path/{name}/{age}")
+    public String pathVariableV3(@PathVariable String name, @PathVariable int age) {
+        return "PathVariable 다중 연습 name : " + name + " age : " + age;
+    }
 }
